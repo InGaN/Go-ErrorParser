715f9e69edc7559a4fc2828212668a46e48da2fc
InGaN <kevin.van.der.vleuten@gmail.com>
2016-05-23 14:54:49 +0200

    with directory as path, all plain text .txt files are parsed for given tags

--- dataCheck.go
+++ dataCheck.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"mime"
 	"os"
 	s "strings"
 	"time"
 )
+
+var i = 0
+var pTotalAmount *int = &i
+var x = 0
+var pTotalFiles *int = &x
+var buffer bytes.Buffer
 
 func checkFile(e error) {
 	if e != nil {
@@ -21,10 +30,8 @@ func timeTrack(start time.Time, name string) {
 	fmt.Printf("%s took %s\n", name, elapsed)
 }
 
-func checkContainsScanner(tags []string) bool {
+func checkContainsScanner(tags []string) {
 	defer timeTrack(time.Now(), "Scanner")
-	var val bool = false
-	var amount int = 0
 
 	file, err := os.Open(os.Args[1])
 	if err != nil {
@@ -35,90 +42,76 @@ func checkContainsScanner(tags []string) bool {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		input := scanner.Text()
-		for _, element := range tags {
-			if s.Contains(input, element) {
-				val = true
-				//fmt.Printf("tag: %s | %s \n\n", tags[index], input)
-				amount++
-			}
-		}
-	}
+		parseLine(input, tags)
+	}	
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Printf("\namount of errors: %d\n", amount)
-
-	return val
 }
 
-func checkContainsReadString(tags []string) bool {
-	defer timeTrack(time.Now(), "ReadString")
-	var val bool = false
-	var amount int = 0
+func checkFiles(path string) {
+	defer timeTrack(time.Now(), "Operation")
+	mimeTxt := mime.TypeByExtension(".txt")
+	tags := []string{"ERROR", "Error", "error", "Warning"}
+	
+	files, _ := ioutil.ReadDir(path)
+	for _, file := range files {
+		if(mime.TypeByExtension(file.Name()[len(file.Name())-4:]) == mimeTxt) {
+			//fmt.Printf("Type: %s\n", file.Name())
+			checkContainsReadString(fmt.Sprintf("%s\\%s",path, file.Name()), tags)
+		}				
+	}	
+}
 
-	file, err := os.Open(os.Args[1])
+func checkContainsReadString(path string, tags []string) {
+	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
-		//return &stats, err
 	}
-
+	changeFlag := *pTotalAmount
+	
 	r := bufio.NewReader(file)
 	for {
 		recordRaw, err := r.ReadString('\n')
 		if err == io.EOF {
 			break
 		}
-		for _, element := range tags {
-			if s.Contains(recordRaw, element) {
-				val = true
-				//fmt.Printf("tag: %s | %s \n\n", tags[index], recordRaw)
-				amount++
-			}
+		parseLine(recordRaw, tags)
+	}			
+	if(changeFlag < *pTotalAmount) {
+		buffer.WriteString(fmt.Sprintf("%s\n", file.Name()))
+		*pTotalFiles++
+	}
+	file.Close()
+}
+
+func parseLine(input string, tags []string) {
+	amount := 0
+	for index, element := range tags {
+		if s.Contains(input, element) {
+			fmt.Printf("tag: %s | %s \n", tags[index], input)
+			amount++
 		}
 	}
-	fmt.Printf("\namount of errors: %d\n", amount)
-	file.Close()
-
-	return val
+	*pTotalAmount = *pTotalAmount + amount
 }
+
 
 func main() {
 	if len(os.Args) != 2 {
 		os.Exit(1)
-	}
+	}	
 
+	
+	
+	checkFiles(os.Args[1])
+	
 	t := time.Now()
+	fmt.Printf("%d-%02d-%02d %d:%d:%d\n", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+	fmt.Printf("total amount of errors found: %d\n", *pTotalAmount)
+	fmt.Printf("in %d files:\n", *pTotalFiles)
+	fmt.Printf(buffer.String())
+	
 
-	fmt.Println("== Starting data checker ==")
-	fmt.Printf("%d-%02d-%02d %d:%d:%d\n\n", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
-
-	tags := []string{"ERROR", "Error", "error", "Warning"}
-
-	if checkContainsReadString(tags) {
-		fmt.Println("ERRORS FOUND")
-	} else {
-		fmt.Println("NO ERRORS FOUND")
-	}
-	if checkContainsScanner(tags) {
-		fmt.Println("ERRORS FOUND")
-	} else {
-		fmt.Println("NO ERRORS FOUND")
-	}
-	if checkContainsReadString(tags) {
-		fmt.Println("ERRORS FOUND")
-	} else {
-		fmt.Println("NO ERRORS FOUND")
-	}
-	if checkContainsScanner(tags) {
-		fmt.Println("ERRORS FOUND")
-	} else {
-		fmt.Println("NO ERRORS FOUND")
-	}
-	if checkContainsReadString(tags) {
-		fmt.Println("ERRORS FOUND")
-	} else {
-		fmt.Println("NO ERRORS FOUND")
-	}
 	// TXT file parse concept
 }
